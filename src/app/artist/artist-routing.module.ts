d63faa5e1eae5a68d9503ea7a47219a5b886d7f7
Amresh Prasad <amresh1098@gmail.com>
2023-03-13 19:07:48 +0530

    feat:Artist-Description-Page-Design completed

--- src/app/artist/artist-routing.module.ts
+++ src/app/artist/artist-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtistDescriptionContainerComponent } from './artist-description-container/artist-description-container.component';
 import { ArtistComponent } from './artist.component';
 
-const routes: Routes = [{ path: '', component: ArtistComponent }];
+const routes: Routes = [{ path: '', component: ArtistComponent,
+  children:[
+    {
+      path:'artist-description',component:ArtistDescriptionContainerComponent
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
