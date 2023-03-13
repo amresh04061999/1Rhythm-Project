be8999771f661893a1d161b819c5d74d76887e98
Amresh Prasad <amresh1098@gmail.com>
2023-03-13 17:39:10 +0530

    feat:studio Description page design completed

--- src/app/studio/studio-routing.module.ts
+++ src/app/studio/studio-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudioDescriptionContainerComponent } from './studio-description-container/studio-description-container.component';
 import { StudioComponent } from './studio.component';
 
-const routes: Routes = [{ path: '', component: StudioComponent }];
+const routes: Routes = [{ path: '', component: StudioComponent,
+   children:[
+    {
+      path:'studio-description',component:StudioDescriptionContainerComponent
+    }
+   ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
