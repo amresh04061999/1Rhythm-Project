733bbb050efc0aaf96f9c50a65a57c33a20c372f
Amresh Prasad <amresh1098@gmail.com>
2023-03-20 19:01:27 +0530

    feat:togale completed

--- src/app/studio/manage-studio-profile-container/manage-studio-profile-presentation/manage-studio-profile-presentation.component.ts
+++ src/app/studio/manage-studio-profile-container/manage-studio-profile-presentation/manage-studio-profile-presentation.component.ts
@@ -3,26 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-manage-studio-profile-presentation',
   templateUrl: './manage-studio-profile-presentation.component.html',
-  styleUrls: ['./manage-studio-profile-presentation.component.scss']
+  styleUrls: ['./manage-studio-profile-presentation.component.scss'],
 })
 export class ManageStudioProfilePresentationComponent {
-  public userTypes=[
-    { id:1,name:'General User'},
-    { id:2,name:'Studio Owner'},
-    { id:3,name:'Artist'}
-    ]
-    public checkValue:string;
-    public checkValue1:boolean;
+  public userTypes = [
+    { id: 1, name: 'General User' },
+    { id: 2, name: 'Studio Owner' },
+    { id: 3, name: 'Artist' },
+  ];
+  public checkValue: boolean;
+  public checkValue1: boolean;
+  public checkValue2: boolean;
 
-    constructor(){
-      this.checkValue='';
-      this.checkValue1=true;
-
-    }
-   public  checkBox(event:any) :void{
-    console.log(event);
-    
-   this.checkValue=event.target.id;
-    this.checkValue1=event.target.checked;
-    }
+  constructor() {
+    this.checkValue = false;
+    this.checkValue1 = false;
+    this.checkValue2 = false;
+  }
+  public checkBox( value: any): void {
+    if (value === 'RecordingRoom') {
+      this.checkValue = !this.checkValue;
+    } else if (value === 'JammingRoom') {
+      this.checkValue1 = !this.checkValue1;
+    } else if (value === 'Mixing') {
+      this.checkValue2 = !this.checkValue2;
+    } 
+  }
 }
