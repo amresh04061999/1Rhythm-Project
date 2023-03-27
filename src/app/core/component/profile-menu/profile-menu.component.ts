943d7581ebad9ed3783f3cf243ee2b5923d810b1
amresh prasad <amresh04061999@gmail.com>
2023-03-27 07:52:17 +0530

    feat:filter depend on userroll

--- src/app/core/component/profile-menu/profile-menu.component.ts
+++ src/app/core/component/profile-menu/profile-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 import { OverlayService } from '../../services/overlay/overlay.service';
@@ -8,14 +8,22 @@ import { OverlayService } from '../../services/overlay/overlay.service';
   templateUrl: './profile-menu.component.html',
   styleUrls: ['./profile-menu.component.scss']
 })
-export class ProfileMenuComponent {
+export class ProfileMenuComponent implements OnInit {
+  public getSingleUser: any;
   constructor(private _userService: AuthService, private overlayServec: OverlayService, private router: Router) {
 
+  }
+  ngOnInit(): void {
+    this.getSingleusers()
   }
   public logOut() {
     this._userService.logout();
     this.overlayServec.close();
     this.router.navigateByUrl('/login')
+  }
+  public getSingleusers() {
+    this.getSingleUser = this._userService.userget();
+    this.getSingleUser = JSON.parse(this.getSingleUser)
   }
 
 }
