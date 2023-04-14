a960750d578ea96e58e6ed7c1f85f64b67e3e346
Amresh Prasad <amresh1098@gmail.com>
2023-04-14 13:31:08 +0530

    feat:upafte

--- src/app/studio/manage-studio-profile-container/manage-Studio-profile-presenter/manage-studio-profile-presenter.service.ts
+++ src/app/studio/manage-studio-profile-container/manage-Studio-profile-presenter/manage-studio-profile-presenter.service.ts
@@ -1,7 +1,9 @@
 import { NgSwitch } from '@angular/common';
 import { Injectable } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
+import { City, Country, State } from 'src/app/shared/model/location.model';
+import { Studio } from '../../model/studio';
 
 @Injectable()
 export class ManageStudioProfilePresenterService {
@@ -14,8 +16,13 @@ export class ManageStudioProfilePresenterService {
   private multipleImage: Subject<any>;
   public checkValueArray: Subject<any>;
   public multipleImage$: Observable<any>;
-  public id:number;
-  constructor(private fb:FormBuilder) {
+  public filterState: Subject<State[]>;
+  public filterState$: Observable<State[]>;
+  public filterCity: Subject<City[]>;
+  public filterCity$: Observable<City[]>;
+  public StudioDetails: Subject<any>;
+  public StudioDetails$: Observable<any>;
+  constructor(private fb: FormBuilder) {
     this.imageArray = [];
     this.multipleImage = new Subject();
     this.checkValueArray = new Subject();
@@ -23,44 +30,50 @@ export class ManageStudioProfilePresenterService {
     this.checkValue = false;
     this.checkValue1 = false;
     this.checkValue2 = false;
-    this.id=1;
+    this.filterState = new Subject();
+    this.filterState$ = this.filterState.asObservable();
+    this.filterCity = new Subject();
+    this.filterCity$ = this.filterCity.asObservable();
+    this.StudioDetails = new Subject();
+    this.StudioDetails$ = this.StudioDetails.asObservable();
   }
-
-/**
- * form builder create and return all data 
- * @returns 
- */
-  public buildForm():FormGroup{
+  /**
+   * form builder create and return all data
+   * @returns
+   */
+  public buildForm(): FormGroup {
     return this.fb.group({
-          personalDetails:this.fb.group({
-            id:[''],
-            studioName:[''],
-            address:[''],
-            studioDescription:[''],
-            hardwareEquipments:[''],
-          }),
-          location:this.fb.group({
-            country:[''],
-             state:[''],
-             city:[''],
-          }),
-            
-          days:this.fb.group({
-            fromDay:[''],
-            toDay:[''],
-          }),
-          hours:this.fb.group({
-            fromHour:[''],
-             toHour:[''],
-          }),
-          studioServices:this.fb.group({
-            recordingAmount:[''],
-            jammingAmount:[''],
-            MasteringAmount:[''],
-          }),
-            
-            
-      })
+         id: [''],
+      personalDetails: this.fb.group({
+        studioName: [''],
+        address: [''],
+        studioDescription: [''],
+        hardwareEquipments: [''],
+      }),
+      location: this.fb.group({
+        countryId: [''],
+        stateId: [''],
+        cityId: [''],
+      }),
+
+      days: this.fb.group({
+        fromDay: [''],
+        toDay: [''],
+      }),
+      hours: this.fb.group({
+        fromHour: [''],
+        toHour: [''],
+      }),
+      studioServices: this.fb.group({
+        serviceTitle:[''],
+        recordingAmount:[''],
+        jammingAmount:[''],
+        MasteringAmount:[]
+
+      }),
+
+      studioImages: this.fb.array([])
+    });
   }
 
   /**
@@ -75,39 +88,88 @@ export class ManageStudioProfilePresenterService {
       render.readAsDataURL(this.imageFile);
       render.onload = () => {
         this.base64 = render.result;
-        this.id++
-        this.imageArray.push({id:this.id,image: this.base64 });
-        console.log(this.imageArray);
-        
+        this.imageArray.push({  isLike:false, image: this.base64 });
       };
     });
     this.multipleImage.next(this.imageArray);
   }
-/**
- *  
- * @param id 
- */
+  /**
+   *
+   * @param id
+   */
   public selectCheckBox(id: any) {
     if (id === 'RecordingRoom') {
-      this.checkValue = !this.checkValue
+      this.checkValue = !this.checkValue;
     } else if (id === 'JammingRoom') {
       this.checkValue1 = !this.checkValue1;
     } else if (id === 'Mixing') {
       this.checkValue2 = !this.checkValue2;
     }
-    this.checkValueArray.next([this.checkValue,this.checkValue1,this.checkValue2])
-  } 
+    this.checkValueArray.next([
+      this.checkValue,
+      this.checkValue1,
+      this.checkValue2,
+    ]);
+  }
+
+  /**
+   * remove image using index
+   * @param id
+   */
+  public removeImageById(id: number) {
+    const b = this.imageArray.splice(id, 1);
+  }
+
+
+public setFavImage(selectedIndex:number){
+  this.imageArray.find((image:any)=>image.isLike=true);
+  this.imageArray=this.imageArray.map((item:any,index:number)=>{
+    if(index === selectedIndex){
+      item.isLike=true
+    }else{
+      item.isLike=false
+    }
+    return item
+  })  
+}
 
 /**
- * remove image using index
- * @param id 
+ * 
+ * @param studioDetails 
+ * @returns 
  */
-  public removeImageById(id:number){
-    const b = this.imageArray.splice(id,1)
+  public saveStudioDetails(studioDetails: FormGroup) {
+    const image= studioDetails.controls['studioImages'] as FormArray
+    image.push(this.fb.group(this.imageArray))
+    if (!studioDetails.valid) {
+      return;
+    }
+    this.StudioDetails.next(studioDetails.value);
   }
-  
-  public saveStudioDetails(){
+  /**
+   * Filter state Depend  of country
+   * @param item
+   * @param currentId
+   */
+  public changeState(state: any, currentId: number) {
+    const countryId = currentId;
+    const filterState = state.filter(
+      (item: State) => item.countryId == countryId
+    );
+    this.filterState.next(filterState);
+  }
+
+  /**
+   * Filter city Depend of State
+   * @param item
+   * @param currentId
+   */
+  public changeCity(city: any, currentId: number) {
+    const stateId = currentId;
+    const filterCity = city.filter((item: City) => item.stateId == stateId);
+    this.filterCity.next(filterCity);
+  }
+  public weekChange(id:number){
 
   }
-
 }
--- src/app/studio/manage-studio-profile-container/manage-studio-profile-presentation/manage-studio-profile-presentation.component.ts
+++ src/app/studio/manage-studio-profile-container/manage-studio-profile-presentation/manage-studio-profile-presentation.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { ManageStudioProfilePresenterService } from '../manage-Studio-profile-presenter/manage-studio-profile-presenter.service';
 import { FormGroup } from '@angular/forms';
 import { City, Country, State } from 'src/app/shared/model/location.model';
+import { retry } from 'rxjs';
 
 @Component({
   selector: 'app-manage-studio-profile-presentation',
@@ -15,32 +16,80 @@ export class ManageStudioProfilePresentationComponent implements OnInit {
     { id: 2, name: 'Studio Owner' },
     { id: 3, name: 'Artist' },
   ];
-  @Input() set getCountry(Response: Country[]| null) {
-    if (Response) {
-      this._getCountry=Response
-    }
+  
+  @Input() set getCountry(Response: Country[] | null) {
+   if(Response){
+     this._getCountry=Response    
+  
+   }
+  }
+  public get getCountry(){
+    return this._getCountry
   }
   @Input() set getState(Response: State[] | null) {
     if (Response) {
     this._getState=Response
     }
   }
+  public get getState(){
+    return this._getState
+  }
   @Input() set getCity(Response: City[] | null) {
     if (Response) {
      this._getCity=Response
     }
   }
+  public get getCity(){
+    return this._getCity
+  }
   public imageArray:any;
   public base64:any;
   public checkValueArray=[];
   public studioForm:FormGroup;
-  private _getCountry!:Country[];
+  public _getCountry!:Country[];
   private _getState!:State[];
+  public   getStates!:any;
+  public   getCities!:City[];
   private _getCity!:City[];
+  public addStudioDetails:EventEmitter<any>;
+
+   public days= [
+    { id: 1, day: 'Monday' },
+    { id: 2, day: 'Tuesday' },
+    { id: 3, day: 'Wednesday' },
+    { id: 4, day: 'Thursday' },
+    { id: 5, day: 'Friday' },
+    { id: 6, day: 'Saturday' },
+    { id: 7, day: 'Sunday' },
+  ];
+  public hours= [
+    { id: 1, hour: '12Am' },
+    { id: 2, hour: '1Am' },
+    { id: 3, hour: '2Am' },
+    { id: 4, hour: '3Am' },
+    { id: 5, hour: '4Am' },
+    { id: 6, hour: '5Am' },
+    { id: 7, hour: '6Am' },
+    { id: 9, hour: '7Am' },
+    { id: 10, hour: '8Am' },
+    { id: 11, hour: '9Am' },
+    { id: 12, hour: '10Am' },
+    { id: 13, hour: '11Am' },
+    { id: 14, hour: '1Ppm' },
+    { id: 15, hour: '2Pm' },
+    { id: 16, hour: '3Pm' },
+    { id: 17, hour: '4Pm' },
+    { id: 18, hour: '5Pm' },
+    { id: 19, hour: '6Pm' },
+    { id: 20, hour: '7Pm' },
+    { id: 21, hour: '9Pm' },
+    { id: 22, hour: '10Pm' },
+    { id: 23, hour: '11Pm' },
+  ]
   constructor(private manageStudioProfilePresenterService:ManageStudioProfilePresenterService) {
     this.imageArray=[];
-
-    this.studioForm=this.manageStudioProfilePresenterService.buildForm()
+    this.studioForm=this.manageStudioProfilePresenterService.buildForm(); 
+    this.addStudioDetails=new EventEmitter()   
   }
 
   ngOnInit(): void {
@@ -50,9 +99,32 @@ export class ManageStudioProfilePresentationComponent implements OnInit {
     this.manageStudioProfilePresenterService.multipleImage$.subscribe((res)=>{
           this.imageArray=res            
     })
+    /**
+     * get array of checkbox
+     */
     this.manageStudioProfilePresenterService.checkValueArray.subscribe((res)=>{
        this.checkValueArray=res     
     })
+    /**
+     * get filter state
+     */
+    this.manageStudioProfilePresenterService.filterState$.subscribe((res)=>{
+      this.getStates=res     
+   })
+     /**
+     * get filter city
+     */
+     this.manageStudioProfilePresenterService.filterCity$.subscribe((res)=>{
+      this.getCities=res     
+   })
+    /**
+     * Send studio details in container using Emitter
+     */
+this.manageStudioProfilePresenterService.StudioDetails$.subscribe((res)=>{
+       console.log(res);
+       this.addStudioDetails=res
+})
+
   }
   public checkBox( value: any): void {
     this.manageStudioProfilePresenterService.selectCheckBox(value); 
@@ -72,8 +144,34 @@ export class ManageStudioProfilePresentationComponent implements OnInit {
    public removeImage(id:number):void {
     this.manageStudioProfilePresenterService.removeImageById(id);
    }
-   public saveStudio():void{
-     this.manageStudioProfilePresenterService.saveStudioDetails()
+
+   public setFavImage(index:number){
+    this.manageStudioProfilePresenterService.setFavImage(index)
    }
-   
+   /**
+    * 
+    */
+   public saveStudio():void{
+     this.manageStudioProfilePresenterService.saveStudioDetails(this.studioForm)
+   }
+   /**
+    * 
+    * @param event 
+    */
+   public changeState(event:number){
+      this.studioForm.get('location')?.get('stateId',)?.reset();
+      const a = this.studioForm.get('location')?.get('cityId')?.reset()
+      this.manageStudioProfilePresenterService.changeState(this.getState,event)
+    }
+    /**
+     * 
+     * @param event 
+    */
+   public changeCity(event:number){
+     const a = this.studioForm.get('location')?.get('cityId')?.reset()
+    this.manageStudioProfilePresenterService.changeCity(this.getCity,event)
+   }
+public weekChange(id:number){
+  this.manageStudioProfilePresenterService.weekChange(id)
+}
 }
